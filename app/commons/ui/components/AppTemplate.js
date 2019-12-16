61e6d82fa5a25c4aa097b926c5d76dfcbec05bf8
vinodh1988 <k.c.vinodh@hotmail.com>
2019-12-16 11:13:56 +0530

    first commit page

---
+++ app/commons/ui/components/AppTemplate.js
@@ -0,0 +1,23 @@
+import React, {PureComponent} from 'react';
+import {Link} from 'react-router-dom';
+import basePath from 'commons/utils/basePath';
+
+export default ({children}) => (
+  <div className="flex-container">
+    <nav>
+      <Link to={basePath``} >Home</Link>
+      <Link to={basePath`projects`}>Projects</Link>
+      <Link to={basePath`contact`} >Contact</Link>
+    </nav>
+    <div className="container">
+      <div className="card">
+        
+        {children}
+      </div>
+    </div>
+    <hr/>
+    <footer style={{textAlign: "center"}}>
+      <b>Lazy Routing Demo</b>
+    </footer>
+  </div>
+);
